b42851005b75447cc98ee695e57cc29a39e136c3
RajatRawat10 <rajatrawat4242@gmail.com>
2025-11-13 23:28:20 +0530

    Update EmailJS integration: fixed form fields and added auto-reply

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const form = document.getElementById("contact-form");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    emailjs.sendForm("service_uunsmsw", "template_i2nrxq6", this)
+    emailjs.sendForm("service_fortherajat78123", "template_7jphqsi", this)
         .then(() => {
             alert("✅ Message Sent Successfully!");
             form.reset();
